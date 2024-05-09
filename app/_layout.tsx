174d94d367a675765eb62084cd1fbe17d2fa9664
juan.jaramillo <juan.jaramillo@rappi.com>
2024-05-09 01:03:13 -0500

    add: interview challenge code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,51 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { Back, QueryProvider, Text } from "@components";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useLoadI18n } from "@hooks";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { LogBox, useColorScheme } from "react-native";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+LogBox.ignoreLogs(["Require cycle"]);
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "index",
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync().catch(console.error);
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+  const [fontLoaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    PreloSlab: require("../assets/fonts/PreloSlab-Bold.ttf"),
+    ...AntDesign.font,
   });
+  const { i18nLoaded, error: i18Error } = useLoadI18n();
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  const areAssetsLoading = !fontLoaded || !i18nLoaded;
+
   useEffect(() => {
     if (error) throw error;
-  }, [error]);
+    if (i18Error) throw i18Error;
+  }, [error, i18Error]);
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (!areAssetsLoading) {
+      SplashScreen.hideAsync().catch(console.error);
     }
-  }, [loaded]);
+  }, [areAssetsLoading]);
 
-  if (!loaded) {
+  if (areAssetsLoading) {
     return null;
   }
 
@@ -46,13 +54,47 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
-
+  const { t } = useTranslation();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <QueryProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerTitle: () => <Text variant="title">{t("home.title")}</Text>,
+            }}
+          />
+          <Stack.Screen
+            name="details/[id]"
+            options={{
+              headerTitle: () => (
+                <Text variant="title">{t("details.title")}</Text>
+              ),
+              headerBackVisible: false,
+              headerLeft: () => <Back />,
+            }}
+          />
+          <Stack.Screen
+            name="forms/financialProductForm"
+            options={{
+              headerTitle: () => <Text variant="title">{t("form.title")}</Text>,
+              headerBackVisible: false,
+              headerLeft: () => <Back />,
+            }}
+          />
+          <Stack.Screen
+            name="utils/success"
+            options={{
+              headerTitle: () => (
+                <Text variant="title">{t("success.title")}</Text>
+              ),
+              headerBackVisible: false,
+              headerLeft: () => <Back />,
+            }}
+          />
+        </Stack>
+      </QueryProvider>
     </ThemeProvider>
   );
 }
